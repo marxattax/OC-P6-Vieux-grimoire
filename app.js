3b566ae6bb34b78ed5ec4e07ea7573e958178216
marxattax <cuisy.vincent@gmail.com>
2024-09-24 13:02:14 +0200

    delete deprecated options

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express()
-app.use(express.json())
 const path = require('path');
 require('dotenv').config()
 
@@ -9,6 +8,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB}:${process.env.MONGOPWD}@c
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
