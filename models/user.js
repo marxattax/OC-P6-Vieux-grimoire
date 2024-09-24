a3866ac92d756659bb746056f733028cd57011cc
marxattax <cuisy.vincent@gmail.com>
2024-09-24 13:15:41 +0200

    rajout validation email

--- models/user.js
+++ models/user.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+const validateEmail = function (email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email.trim());
+};
+
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: {
+        type: String, 
+        required: true, 
+        validate: [validateEmail, "{VALUE} n'est pas un email valide"],
+        unique: true 
+    },
+    password: { 
+        type: String,
+        required: true 
+    }
 });
 
 userSchema.plugin(uniqueValidator);
